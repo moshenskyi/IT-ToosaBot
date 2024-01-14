83298a5351661d24820d7609f694f60c06794850
moshenskyi <moshenskyi.nazariy@gmail.com>
2024-01-14 21:11:54 +0200

    Add integration for pythonanywhere

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import os
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+from aiogram.client.session.aiohttp import AiohttpSession
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=os.getenv("TOOSA_BOT_TOKEN"))
+
+session = AiohttpSession(proxy='http://proxy.server:3128')
+bot = Bot(token=os.getenv("TOOSA_BOT_TOKEN"), session=session)
 dp = Dispatcher()
+
 
 @dp.message(F.new_chat_members)
 async def delete_new_member_msg(message: Message):
@@ -19,8 +23,9 @@ async def delete_new_member_msg(message: Message):
 async def delete_left_member_msg(message: Message):
     await message.delete()
 
+
 async def main():
-     await dp.start_polling(bot)
+    await dp.start_polling(bot)
 
 
 # Press the green button in the gutter to run the script.
